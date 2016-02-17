4f37014add3dd53cc8772d756d7955549b985cc8
merey90 <merey.90@gmail.com>
2016-02-17 05:32:30 +0000

    default values to post,comment upvotes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+  before_create :default_values
+  
+  def default_values
+    self.upvotes ||= 0
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,11 @@
 class Post < ActiveRecord::Base
     has_many :comments
-    
+    before_create :default_values
+  
+    def default_values
+        self.upvotes ||= 0
+    end
+  
     def as_json(options = {})
         super(options.merge(include: :comments))
     end
